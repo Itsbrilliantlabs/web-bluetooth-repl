0750f3c1ad1c2d6f4733096858877611f3b276b1
te-uma <umasankar@technoexponent.com>
2023-02-07 17:07:45 +0530

    deploy scripts and naming of conrtainer, readme and other files added

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 docker-compose -f docker-compose.prod.yaml build
 docker-compose -f docker-compose.prod.yaml down
-docker volume remove webrepl_server_static_volume
+docker volume remove webrepl_static
 docker-compose -f docker-compose.prod.yaml up -d
 docker system prune --all --force
