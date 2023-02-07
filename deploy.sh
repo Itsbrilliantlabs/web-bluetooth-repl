869c730699b13756111a07ad4d631fb87e34bba3
te-uma <umasankar@technoexponent.com>
2023-02-07 17:43:28 +0530

    adjusted to working under 3.3 docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-git pull origin/dev_server
-git checkout origin/dev_server
+git pull origin dev_server
+git checkout origin dev_server
 docker-compose -f docker-compose.prod.yaml build
 docker-compose -f docker-compose.prod.yaml down
-docker volume remove webrepl_static
+docker volume remove web-bluetooth-repl_static_volume
 docker-compose -f docker-compose.prod.yaml up -d
 docker system prune --all --force
