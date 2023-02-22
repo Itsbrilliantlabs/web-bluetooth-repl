c6eb306f12ad74ea430816692b197bdf833d7483
te-uma <umasankar@technoexponent.com>
2023-02-23 05:02:27 +0530

    added platform check

--- js/terminal.js
+++ js/terminal.js
@@ -852,3 +852,33 @@ window.startMonocleFirmwareUpdate = () => {
 
 //     }
 // })
+
+
+// from https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+  
+    return os;
+  }
+
+  if(getOS()=="Android"){
+    document.querySelector('.simple-keyboard').style.display="block";
+    replConsole.setAttribute('input-mode','none')
+  }
